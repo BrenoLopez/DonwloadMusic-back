6e51fd2681ee680f52026aca62f8c8cc48710b1b
Breno Lopes <breno.souza@usemobile.xyz>
2021-04-01 16:11:33 -0300

    ...

--- src/services/youtubeStream.ts
+++ src/services/youtubeStream.ts
@@ -7,33 +7,39 @@ export default {
   async musicStream(videoId: string, cb: Response) {
     try {
       if (!!videoId) {
-        const info = await ytdl.getBasicInfo(
+        const validateVideoId = ytdl.validateURL(
           `http://www.youtube.com/watch?v=${videoId}`
         );
-        if (!!info) {
+        // const info = await ytdl.getInfo(`http://www.youtube.com/watch?v=${videoId}`);
+        // console.log(info);
+        if (!!validateVideoId) {
+          let streamSize = 0;
           const stream = ytdl(`http://www.youtube.com/watch?v=${videoId}`, {
             highWaterMark: 32,
             quality: "highestaudio",
           });
           cb.writeHead(200, {
+            Connection: "Keep-Alive",
+            "Accept-Ranges": "bytes",
             "Transfer-Encoding": "chunked",
             "Content-Type": "audio/mp3",
-            // "Content-Length": Infinity,
           });
           ffmpeg(stream)
             .setFfmpegPath(pathFfmpeg)
             .audioBitrate("128k")
             .toFormat("mp3")
             .on("error", (error) => {
-              cb.destroy(error);
+              // cb.destroy(error);
+              console.log(error);
             })
             .on("stderr", console.log)
+            .on("progress", console.log)
             .on("end", () => {
               console.log("end");
             })
             .pipe(cb, { end: true });
           //    .save(path.resolve(__dirname,'..','..','downloads',`${videoId}.mp3`));
-        }
+        } else return cb.status(400).json({ error: "Invalid videoId!" });
       }
     } catch (error: any) {
       return cb.status(400).json(error);
